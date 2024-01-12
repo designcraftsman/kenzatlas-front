afaffbf766dfb4d06699d22894c50f99add490b6
Oussama Fayz <fzoussfr@gmail.com>
2024-01-12 23:40:49 +0100

    updated

--- js/productsFilter.js
+++ js/productsFilter.js
@@ -1,25 +1,39 @@
 // Store product categories outside the event handler
 const productCategories = document.querySelectorAll('.shop__container__products__list__product__details__categorie');
+const productStates = document.querySelectorAll('.shop__container__products__list__product__details__etat');
 
 function getCheckedOptions() {
-  const checkboxes = document.querySelectorAll('.form-check-input');
-  const checkedOptions = [];
+  var checkboxes = document.querySelectorAll('.form-check-input');
+  var checkedOptions = [];
 
   checkboxes.forEach(checkbox => {
     if (checkbox.checked) {
       checkedOptions.push(checkbox.value);
     }
   });
+  
+  productStates.forEach((productState , index) => {
+    const product = document.querySelectorAll('.shop__container__products__list__product')[index];
+    console.log(productState.textContent);
+
+    if (checkedOptions.includes(productState.textContent) || checkedOptions.length === 0) {
+      product.classList.remove('d-none');
+    }else {
+      product.classList.add('d-none');
+    }
+  });
+
 
   // Iterate over products once
   productCategories.forEach((productCategorie, index) => {
     const product = document.querySelectorAll('.shop__container__products__list__product')[index];
+
     console.log(productCategorie.textContent);
 
     // Check if the product category is included in checked options
-    if (checkedOptions.includes(productCategorie.textContent)) {
+    if (checkedOptions.includes(productCategorie.textContent) || checkedOptions.length === 0) {
       product.classList.remove('d-none');
-    } else {
+    }else {
       product.classList.add('d-none');
     }
   });
