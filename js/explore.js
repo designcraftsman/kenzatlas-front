c6601070edcbc3b2135a04568563e74f13371a8f
Oussama Fayz <fzoussfr@gmail.com>
2024-01-19 21:06:55 +0100

    added some new products

--- js/explore.js
+++ js/explore.js
@@ -2,7 +2,15 @@
 // index.html
 
 document.addEventListener('DOMContentLoaded', function() {
+    const heroSectionButtons = document.querySelectorAll('.heroSection__container__carousel__item__info__btn');
     const exploreHair = document.getElementById('exploreHair');
+
+
+    heroSectionButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        window.location.href = 'shop.html';
+            });
+    });
   
     exploreHair.addEventListener('click', function() {
       // Navigate to shop.html
